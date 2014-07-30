f2d19913e44b532c421a1548a35a6151e53ccdf3
solostyle <spam@solostyle.net>
2014-07-30 07:52:42 -0600

    resume controller

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
-gem 'rails', '4.1.1'
+gem 'rails', '4.0.2'
 
 group :development do
   gem 'sqlite3', '1.3.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 DemoApp::Application.routes.draw do
-  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'resume#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +13,7 @@ DemoApp::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
